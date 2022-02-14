90e214d5b0fda366129ecc0f6c933fd270280d0a
Sindhu Priya Korrapati <94669334+SindhuPriyaKorrapatiMuni@users.noreply.github.com>
2022-02-14 06:58:17 +0000

    done

--- p3final.c
+++ p3final.c
@@ -2,6 +2,28 @@
 int input_n()
 {
   int n;
-  printf("the sum of n numbers:\n");
-  scanf()
+  printf("enter the number:\n");
+  scanf("%d",&n);
+  return n;
+}
+int sum_n(int n)
+{
+  int i,sum=0;
+  for(i=0;i<=n;i++)
+    {
+      sum=sum+i;
+    }
+  return sum;
+}
+void output(int n,int sum)
+{
+  printf("the sum of n numbers is %d",sum);
+}
+int main()
+{
+  int n,sum,i;
+  n=input_n();
+  sum=sum_n(n);
+  output(n,sum);
+  return 0;
 }
